0609d1b108f739b3d9a3b7ea57151c93587dfffa
Tang Weinan <twn39@163.com>
2018-08-22 12:43:39 +0800

    update

--- src/DatabaseFactory.php
+++ src/DatabaseFactory.php
@@ -1,29 +1,19 @@
 <?php
 
 namespace PHPModules\Database;
-
 use Psr\Container\ContainerInterface;
 use Illuminate\Database\Capsule\Manager as Capsule;
+
 
 class DatabaseFactory
 {
     public function __invoke(ContainerInterface $container)
     {
         $capsule = new Capsule;
+        $config = $container->get('config');
 
-        $capsule->addConnection([
-            'driver'    => getenv('DB_DRIVER'),
-            'host'      => getenv('DB_HOST'),
-            'database'  => getenv('DB_DATABASE'),
-            'username'  => getenv('DB_USERNAME'),
-            'password'  => getenv('DB_PASSWORD'),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ]);
-
+        $capsule->addConnection($config['DB']);
         $capsule->setAsGlobal();
-        $capsule->bootEloquent();
 
         return $capsule;
     }
